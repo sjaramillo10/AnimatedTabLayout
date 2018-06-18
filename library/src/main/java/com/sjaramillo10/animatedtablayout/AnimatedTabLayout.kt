8260a4cb7726ccfa83c7f48d8720830ecbac8d69
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2018-06-18 14:47:59 -0500

    Remove unused tests and dependencies from library

--- library/src/main/java/com/sjaramillo10/animatedtablayout/AnimatedTabLayout.kt
+++ library/src/main/java/com/sjaramillo10/animatedtablayout/AnimatedTabLayout.kt
@@ -4,7 +4,6 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Color
 import android.support.design.widget.TabLayout
-import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.LayoutInflater
